95f2c3dae8c67386bcfe4a1baba4edf1163e2ee0
DarkJutsu <ulloajose34@gmail.com>
2024-08-22 20:04:59 -0600

    init

---
+++ src/main/java/modulo1/semana2/MainValidationPass.java
@@ -0,0 +1,8 @@
+package modulo1.semana2;
+
+public class MainValidationPass {
+    public static void main(String[] args) {
+        String pass="LopezZ";
+        System.out.println(pass);
+    }
+}
