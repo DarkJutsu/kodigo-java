6776bd26d294c505692a01404813299ffc31fb9b
DarkJutsu <ulloajose34@gmail.com>
2024-08-22 20:42:38 -0600

    feat(semana2/):Final ejercicio 1 - validar clave

--- src/main/java/modulo1/semana2/MainValidationPass.java
+++ src/main/java/modulo1/semana2/MainValidationPass.java
@@ -1,8 +1,32 @@
 package modulo1.semana2;
 
+import java.util.Scanner;
+
 public class MainValidationPass {
     public static void main(String[] args) {
-        String pass="LopezZ";
-        System.out.println(pass);
+        int intentos=0;
+        String passVeri ="LopezZ";
+        String pass="";
+        Scanner inputPass=new Scanner(System.in);
+
+        while(!passVeri.equals(pass)){
+            if(intentos<3){
+                System.out.println("Ingrese la clave: ");
+                pass=inputPass.nextLine();
+            }else {
+                System.out.println("Ya no tienes mas intentos");
+                break;
+            }
+
+            if (!passVeri.equals(pass)){
+                System.out.println("Clave incorrecta!!!");
+            }else {
+                System.out.println("La clave es correcta");
+            }
+            intentos++;
+        }
+
+
+
     }
 }
